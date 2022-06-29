2e1780de98df21d6d56576dc514a413d3387d115
tobiisaravia <tttobis@gmail.com>
2022-06-29 01:20:32 -0300

    fix best seller

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 import styles from '../../styles/Product.module.scss'
 
 const Product = ({data}) => {
+
+    function BoxSeller() {
+        if(data.bestseller === 'true') {
+            return(
+                <div className={styles.best_seller_box}>
+                    <p>Best seller</p>
+                </div>
+            )
+        } else {
+            return (<div></div>)
+        }
+    }
+
     return (
         <div className={styles.card}>
             <div className={styles.card_content}>
                 <img src={data.src} alt={data.alt}/>
+                <BoxSeller/>
                 <button className={styles.add_button}>ADD TO CART</button>
             </div>
             <div className={styles.card_info}>
